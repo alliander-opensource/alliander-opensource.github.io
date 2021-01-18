84b58baf71e068d98276436a8785824d38271751
JoepvanGenuchten <joep.van.genuchten@alliander.com>
2021-01-18 14:58:00 +0100

    added documentation for VIVET project

---
+++ DataStandaardenNetbeheerdersVIVET/config.js
@@ -0,0 +1,56 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "ST",
+  publishDate: "2021-01-18",
+  previousPublishDate: "2020-12-31",
+  previousMaturity: "LC",
+  editors: [
+        { name: "Jan Bruinenberg", company: "Alliander", companyURL: "https://www.alliander.com" },
+		{ name: "Ivo Kuijlaars",company: "Enexis", companyURL: "https://www.enexis.nl" },
+		{ name: "Sefanja Severin",company: "Stedin", companyURL: "https://www.stedin.net" },
+		{ name: "Frank Boere",company: "Enduris", companyURL: "https://www.enduris.nl" },
+		{ name: "Richard Flierman",company: "TenneT", companyURL: "https://www.tennet.eu" },
+		{ name: "George Argyropoulos", company: "Stedin", companyURL: "https://www.stedin.net" },
+		{ name: "Joep van Genuchten", company: "Alliander", companyURL: "https://www.alliander.com" },
+      ],
+
+  github: "https://github.com/Alliander/DataStandaardenNetbeheerdersVIVET",
+  issueBase: "https://github.com/Alliander/DataStandaardenNetbeheerdersVIVET/issues/",
+  maxTocLevel: 3,
+  
+  logos: [
+    {
+      src: "https://www.netbeheernederland.nl/_upload/News/Logo_Netbeheer_Nederland_RGB-1244.jpg?mode=crop&h=768&scale=both&w=1600",
+      url: "www.https://www.netbeheernederland.nl/",
+      alt: "Netbeheer Nederland",
+      width: 200,
+      height: 140,
+      id: "nbn-logo",
+    },
+  ],
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+  localBiblio: {
+	  "Station": {
+      "href": "https://alliander.poolparty.biz/AssetManagement/29",
+      "title": "Station",
+      "publisher": "Alliander",
+    },
+	"minbesluit": {
+      "href": "https://zoek.officielebekendmakingen.nl/stb-2020-511.html",
+      "title": "Besluit van 1 december 2020 tot wijziging van het Besluit investeringsplan en kwaliteit elektriciteit en gas (uitvalsituaties hoogspanningsnet)",
+      "authors": [""],
+      "date": "14-12-2020",
+      "publisher": "Ministerie van Economische Zaken en Klimaat",
+    },
+	
+  }
+  
+};
